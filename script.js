4457f622bcfc5347db80c99c6166138b4e575c79
Brun00liveira <brunobromo321@gmail.com>
2023-12-13 19:07:49 -0300

    treino de mouse

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const quadrado = document.getElementById("meuQuadrado");
+    const contadorElement = document.getElementById("contador");
+
+    let tempoMouseDentro = 0;
+    let movimentoInterval;
+    let contadorInterval;
+
+    function moverQuadrado() {
+        const maxX = window.innerWidth - quadrado.clientWidth;
+        const maxY = window.innerHeight - quadrado.clientHeight;
+
+        const novoX = Math.floor(Math.random() * maxX);
+        const novoY = Math.floor(Math.random() * maxY);
+
+        quadrado.style.transform = `translate(${novoX}px, ${novoY}px)`;
+    }
+
+    function iniciarContador() {
+        contadorInterval = setInterval(function() {
+            tempoMouseDentro += 100; // Incrementa a contagem em 100 milissegundos
+            contadorElement.innerText = `Tempo: ${tempoMouseDentro / 100}s`;
+        }, 100);
+    }
+
+    function pararContador() {
+        clearInterval(contadorInterval);
+    }
+
+    quadrado.addEventListener("mouseover", function() {
+        iniciarContador();
+        movimentoInterval = setInterval(moverQuadrado, 1000); // Movimento a cada 500 milissegundos (0.5 segundos)
+    });
+
+    quadrado.addEventListener("mouseout", function() {
+        pararContador();
+        clearInterval(movimentoInterval);
+    });
+
+    // Mover o quadrado inicialmente
+    moverQuadrado();
+});
